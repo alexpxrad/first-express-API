4ca0a922382d6807a273eeb8972d310abe7ff32b
Alexander Pelta <abp06c@Alexanders-MacBook-Air.local>
2022-07-25 15:09:44 -0400

    first express

---
+++ index.js
@@ -0,0 +1,23 @@
+
+//import express library
+import express from 'express';
+
+//initialize express server
+const app = express();
+
+
+//list requests that we want express to respond to 
+app.get('/hello', (request, response) => {
+    //do something...
+    response.send('Hello World 🌎!');
+
+}) 
+
+
+
+
+//start listening on a port
+app.listen(3001, () => {
+    console.log('Now listening on port 3001')
+});
+
